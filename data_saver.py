41ef212342fbd9e629c3fcc24c7f550a1aa2c626
przemyslvw <przemyslvw@gmail.com>
2024-01-22 16:48:04 +0100

    unnecessary code

--- data_saver.py
+++ data_saver.py
@@ -1,4 +1,3 @@
-import random
 import requests
 import json
 import datetime
--- run.py
+++ run.py
@@ -1,7 +1,5 @@
-import json
 from data_saver import save_data_to_file
 from random_lotto import get_random_numbers
-from collections import Counter
 
 save_data_to_file()
 print(get_random_numbers())
